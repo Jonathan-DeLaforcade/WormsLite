d62a40963359e87a726a2f4b77fe986ca06d0075
De Laforcade Jonathan <delaforcade.jonathan@gmail.com>
2019-12-31 20:18:34 +0100

    trajectoire balistique OK

--- roquette.py
+++ roquette.py
@@ -13,28 +13,23 @@ class roquette(object):
         print (" x: "+ str(self.x) + " y: " + str(self.y))
     def tir(self,angle):
 
-        vitesse = 15
+        vitesse = 17
         t = 0
+
         print("Angle: " + str(angle))
-        
-        while t< 60:
-            self.y = self.y + ((((-(1/2))*9.81*t**2)+(sin(angle)*vitesse*t)) * (-1))
+
+        while self.y < 400 :
+            self.y = self.y + (((-1/2)*(9.81)*t**2+sin(angle)*vitesse*t) * (-1))
             self.x = self.x + cos(angle)*vitesse*t
-
-            #if (self.x >= 300):
-                #break
-
-            #self.y -= 5 #hauteur
-            #self.x += 5 #largeur
             t = t + 0.1
-            print ("c1: " + str(((-(1/2))*9.81*t**2)+(sin(angle)*vitesse*t)))
-            """self.fenetre.coords(self.obu,self.x-self.rayon/2,self.y-self.rayon/2, self.x+self.rayon/2, self.y+self.rayon/2)"""
-            self.fenetre.create_oval(self.x-self.rayon/2,self.y-self.rayon/2, self.x+self.rayon/2, self.y+self.rayon/2, fill=self.couleur,width = self.rayon)
+            
+            print ("T: " + str(t) + "X: " + str(self.x) + " Y: " + str(self.y))
+            self.fenetre.coords(self.obu,self.x-self.rayon/2,self.y-self.rayon/2, self.x+self.rayon/2, self.y+self.rayon/2)
+            #self.fenetre.create_oval(self.x-self.rayon/2,self.y-self.rayon/2, self.x+self.rayon/2, self.y+self.rayon/2, fill=self.couleur,width = self.rayon)
             self.fenetre.update()
             sleep(0.1)
-        
-        
-       
+
+        self.Destroy()
     def CreateRoquette(self):
         self.obu = self.fenetre.create_oval(self.x-self.rayon/2,self.y-self.rayon/2, self.x+self.rayon/2, self.y+self.rayon/2, fill=self.couleur,width = self.rayon)
     def Destroy(self):
